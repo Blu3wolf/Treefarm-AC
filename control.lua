92edbb40a1ac7e3222ad4073f11650cd72934fee
Blu3wolf <blue@blu3wolf.com>
2016-01-13 09:32:10 +0800

    update to 0.2.2

--- control.lua
+++ control.lua
@@ -2,7 +2,7 @@ require "defines"
 
 
 
-game.on_event(defines.events.on_built_entity, function(event)
+script.on_event(defines.events.on_built_entity, function(event)
 	if event.created_entity.name == "tf-cokery-dummy" then
 		game.get_surface("nauvis").create_entity{name = "tf-cokery", position = event.created_entity.position, force = game.forces.player}
 		event.created_entity.destroy()
